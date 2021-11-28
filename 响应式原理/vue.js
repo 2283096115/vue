ffa16d97bf8bf4b3e23b52482c201d53cc671dc8
wuwei <wuwei@wezhuiyi.com>
2021-11-28 22:16:08 +0800

    双向绑定3

--- 响应式原理/vue.js
+++ 响应式原理/vue.js
@@ -78,18 +78,22 @@ Watcher.prototype = {
 
 
 
-function SelfVue (data, el, exp) {
+function SelfVue (options) {
     var self = this;
-    this.data = data;
- 
-    Object.keys(data).forEach(function(key) {
-        self.proxyKeys(key);  // 绑定代理属性
+    this.vm = this;
+    this.data = options.data;
+
+    Object.keys(this.data).forEach(function(key) {
+        self.proxyKeys(key);
     });
-    observe(data);
-    el.innerHTML = this.data[exp];  // 初始化模板数据的值
-    new Watcher(this, exp, function (value) {
-        el.innerHTML = value;
-    });
+
+    observe(this.data);
+    new Compile(options.el, this.vm);
+    return this;
+    // el.innerHTML = this.data[exp];  // 初始化模板数据的值
+    // new Watcher(this, exp, function (value) {
+    //     el.innerHTML = value;
+    // });
     return this;
 }
 
@@ -110,15 +114,62 @@ SelfVue.prototype = {
 }
 
 
-function nodeToFragment (el) {
-    var fragment = document.createDocumentFragment();
-    var child = el.firstChild;
-    while (child) {
-        // 将Dom元素移入fragment中
-        fragment.appendChild(child);
-        child = el.firstChild
+function Compile(el, vm) {
+    this.vm = vm;
+    this.el = document.querySelector(el);
+    this.fragment = null;
+    this.init();
+}
+
+Compile.prototype = {
+    init: function () {
+        if (this.el) {
+            this.fragment = this.nodeToFragment(this.el);
+            this.compileElement(this.fragment);
+            this.el.appendChild(this.fragment);
+        } else {
+            console.log('Dom元素不存在');
+        }
+    },
+    nodeToFragment: function (el) {
+        var fragment = document.createDocumentFragment();
+        var child = el.firstChild;
+        while (child) {
+            // 将Dom元素移入fragment中
+            fragment.appendChild(child);
+            child = el.firstChild
+        }
+        return fragment;
+    },
+    compileElement: function (el) {
+        var childNodes = el.childNodes;
+        var self = this;
+        [].slice.call(childNodes).forEach(function(node) {
+            var reg = /\{\{\s*(.*?)\s*\}\}/;
+            var text = node.textContent;
+            if (self.isTextNode(node) && reg.test(text)) {  // 判断是否是符合这种形式{{}}的指令
+                self.compileText(node, reg.exec(text)[1]);
+            }
+
+            if (node.childNodes && node.childNodes.length) {
+                self.compileElement(node);  // 继续递归遍历子节点
+            }
+        });
+    },
+    compileText: function(node, exp) {
+        var self = this;
+        var initText = this.vm[exp];
+        this.updateText(node, initText);  // 将初始化的数据初始化到视图中
+        new Watcher(this.vm, exp, function (value) { // 生成订阅器并绑定更新函数
+            self.updateText(node, value);
+        });
+    },
+    updateText: function (node, value) {
+        node.textContent = typeof value == 'undefined' ? '' : value;
+    },
+    isTextNode: function(node) {
+        return node.nodeType == 3;
     }
-    return fragment;
 }
 
 // var data = {
