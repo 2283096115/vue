e56180feb8875fc37e66100d09429222d4ac0fc1
wuwei <wuwei@wezhuiyi.com>
2021-12-04 16:54:41 +0800

    实现解析元素中的{{}}和完成v-model指令功能

--- 响应式原理/vue1.js
+++ 响应式原理/vue1.js
@@ -45,33 +45,98 @@ Dep.prototype.notify = function() {
   })
 }
 
-function Watcher(data, exp, cb) {
-  this.data = data
+function Watcher(vm, exp, cb) {
+  this.vm = vm
   this.exp = exp
   this.cb = cb
-  this.value = this.get(data, exp)
+  this.value = this.get(vm, exp)
 }
 
 Watcher.prototype.get = function () {
   Dep.target = this
-  const value = this.data[this.exp]
+  const value = this.vm[this.exp]
   Dep.target = null
   return value
 }
 Watcher.prototype.update = function () {
   const oldVal = this.value
-  const newVal = this.data[this.exp]
+  const newVal = this.vm[this.exp]
   if (oldVal !== newVal) {
-    this.cb()
+    this.cb.call(this.vm, newVal, oldVal)
   }
 }
 
+function Compile(el, vm) {
+  this.vm = vm;
+  this.el = document.querySelector(el);
+  this.fragment = null;
+  this.init();
+}
+
+Compile.prototype = {
+  init: function () {
+    
+    this.compileElement(this.el);
+  },
+  compileElement: function(el) {
+    var reg = /\{\{(.*)\}\}/;
+    var childNodes = el.childNodes
+    Array.from(childNodes).forEach(item => {
+      const text = item.textContent
+      // 如果是node 则需要对其属性遍历，判断是否有指令，如果是text，则需要修改器值，并且需要加入到watcher，如果有子节点，要回调
+      if (item.nodeType == 1) {
+        const attributes = item.attributes
+        Array.from(attributes).forEach(attr => {
+          // const name = attr.name
+          if (attr.name === 'v-model') {
+            item.addEventListener('input', e => {
+              const newValue = e.target.value
+              if (newValue !== this.vm[attr.value]) {
+                this.vm[attr.value] = newValue
+              }
+            })
+          }
+        })
+      } else if(item.nodeType === 3 && reg.test(text)) {
+        item.textContent = this.vm[reg.exec(text)[1]]
+        new Watcher(this.vm, reg.exec(text)[1], function(value, oldValue) {
+          item.textContent = value
+        })
+      }
+      if (item.childNodes && item.childNodes.length > 0) {
+        this.compileElement(item)
+      }
+    })
+  }
+}
+
+
 function SelfVue(options) {
-  const {data, el, exp} = options
+  const {data, el} = options
   this.data = data
-  el.innerHTML = data[exp]
+  this.vm = this
+  Object.keys(this.data).forEach(key => {
+    this.proxyKeys(key);
+  });
   observe(data)
-  new Watcher(data, exp, function() {
-    el.innerHTML = data[exp]
-  })
+  new Compile(el, this)
+  // new Watcher(vm, exp, function() {
+  //   el.innerHTML = data[exp]
+  // })
+}
+
+SelfVue.prototype = {
+  proxyKeys: function (key) {
+      var self = this;
+      Object.defineProperty(this, key, {
+          enumerable: false,
+          configurable: true,
+          get: function proxyGetter() {
+              return self.data[key];
+          },
+          set: function proxySetter(newVal) {
+              self.data[key] = newVal;
+          }
+      });
+  }
 }
